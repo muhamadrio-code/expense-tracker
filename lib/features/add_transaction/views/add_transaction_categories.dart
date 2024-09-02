f5909904b61cbf768962824a50cc0f222f84256b
muhammad rio <muhammad.rioperm@gmail.com>
2024-09-02 15:30:33 +0700

    refactor code

--- lib/features/add_transaction/views/add_transaction_categories.dart
+++ lib/features/add_transaction/views/add_transaction_categories.dart
@@ -7,72 +7,70 @@ class _TransactionCategories extends StatelessWidget {
   Widget build(BuildContext context) =>
       BlocBuilder<CategoryBloc, CategoryBlocState>(
         builder: (context, state) {
+          void selectCategory(
+            ({int index, CategoryData categoryData}) data,
+          ) {
+            context.read<CategoryBloc>().add(
+                  SelectCategoryEvent(
+                    category: Category(
+                      type: data.categoryData.type,
+                      name: data.categoryData.name,
+                      index: data.index,
+                    ),
+                  ),
+                );
+          }
+
+          Widget buildCategoryWidget(
+              (int, CategoryData) values, int selectedIndex) {
+            return Builder(builder: (context) {
+              ({int index, CategoryData categoryData}) mValues =
+                  (index: values.$1, categoryData: values.$2);
+              return GestureDetector(
+                onTap: () => selectCategory(mValues),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    DecoratedBox(
+                      decoration: BoxDecoration(
+                        color: values.$1 == selectedIndex
+                            ? Colors.amber
+                            : Colors.transparent,
+                        shape: BoxShape.circle,
+                      ),
+                      child: Icon(
+                        values.$2.iconData,
+                        size: 30,
+                      ),
+                    ),
+                    const SizedBox(height: 4),
+                    Flexible(
+                        child: Text(
+                      values.$2.name,
+                      textAlign: TextAlign.center,
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 2,
+                    ))
+                  ],
+                ),
+              );
+            });
+          }
+
           return GridView.count(
             padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
             shrinkWrap: true,
             mainAxisSpacing: 0,
             crossAxisSpacing: 8,
             crossAxisCount: 4,
-            // childAspectRatio: 2 / 1.5,
-            children: state.categoryType == CategoryType.expense
-                ? _expenseCategoriesGrid(state.categoryIndex)
-                : _incomeCategoriesGrid(state.categoryIndex),
+            children: (state.categoryType == CategoryType.expense
+                    ? expenseCategory
+                    : incomeCategory)
+                .indexed
+                .map<Widget>((e) => buildCategoryWidget(e, state.categoryIndex))
+                .toList(),
           );
         },
       );
-
-  Widget _buildCategoryWidget((int, CategoryData) e, int selectedIndex) {
-    return Builder(builder: (context) {
-      return GestureDetector(
-        onTap: () {
-          context.read<CategoryBloc>().add(
-                SelectCategoryEvent(
-                  category: Category(
-                    type: e.$2.type,
-                    name: e.$2.name,
-                    index: e.$1,
-                  ),
-                ),
-              );
-        },
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            DecoratedBox(
-              decoration: BoxDecoration(
-                color:
-                    e.$1 == selectedIndex ? Colors.amber : Colors.transparent,
-                shape: BoxShape.circle,
-              ),
-              child: Icon(
-                e.$2.iconData,
-                size: 30,
-              ),
-            ),
-            const SizedBox(height: 4),
-            Flexible(
-                child: Text(
-              e.$2.name,
-              textAlign: TextAlign.center,
-              overflow: TextOverflow.ellipsis,
-              maxLines: 2,
-            ))
-          ],
-        ),
-      );
-    });
-  }
-
-  List<Widget> _incomeCategoriesGrid(int selectedIndex) {
-    return incomeCategory.indexed
-        .map<Widget>((e) => _buildCategoryWidget(e, selectedIndex))
-        .toList();
-  }
-
-  List<Widget> _expenseCategoriesGrid(int selectedIndex) {
-    return expenseCategory.indexed
-        .map<Widget>((e) => _buildCategoryWidget(e, selectedIndex))
-        .toList();
-  }
 }
